8b1a0f06f51698898ccec5959bf880189573f9ef
Viktoriia38 <bva922@ukr.net>
2022-12-28 00:52:15 +0200

    Task 1, 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,36 +2,53 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const div = document.querySelector(".gallery");
-const item = galleryItems
-  .map(
-    (obj) => `<a class="gallery__link" href="large-image.jpg">
-    <img
-      class="gallery__image"
-      src="${obj.preview}"
-      data-source="${obj.original}"
-      alt="${obj.description}"
-    />
-  </a>`
-  )
-  .join("");
 
-div.insertAdjacentHTML("beforeend", item);
+function createMarkup(array) {
+  const result = array
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery" <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image lazyload"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a> </div>`;
+    })
+    .join("");
+  return result;
+}
+const items = createMarkup(galleryItems);
+div.insertAdjacentHTML("beforeend", items);
+console.log(items);
+
 div.addEventListener("click", clickOpenImage);
-div.addEventListener("keydown", pressEscToCloseImage);
 
 function clickOpenImage(event) {
   event.preventDefault();
   if (event.target.nodeName !== "IMG") {
     return;
   }
-  const instance = basicLightbox.create(`
-  		<img src="${event.target.dataset.source}">
-  	`);
-  instance.show();
-}
 
-function pressEscToCloseImage(event) {
-  if (event.code === "Escape") {
-    instance.close();
+  const instance = basicLightbox.create(
+    `
+<img src="${event.target.dataset.source}">
+`,
+    {
+      onShow: (instance) => {
+        document.addEventListener("keydown", pressEscToCloseImage);
+      },
+
+      onClose: (instance) => {
+        document.removeEventListener("keydown", pressEscToCloseImage);
+      },
+    }
+  );
+  instance.show();
+  function pressEscToCloseImage(event) {
+    if (event.code === "Escape") {
+      instance.close();
+      console.log(event.code);
+    }
   }
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,9 +5,16 @@ const item = galleryItems
   .map(
     (obj) => `
   <a class="gallery__item" href="${obj.original}">
-  <img class="gallery__image" src="${obj.preview}" alt="${obj.description}" />
+  <img class="gallery__image  lazyload" src="${obj.preview}" alt="${obj.description}" />
 </a>`
   )
   .join("");
 
 div.insertAdjacentHTML("beforeend", item);
+
+new SimpleLightbox(".gallery a", {
+  disableRightClick: true,
+  scrollZoom: false,
+  captionDelay: 250,
+  captionsData: "alt",
+});
